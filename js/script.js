b92eb7822a48bb146b22ddd23e9b0ff74629ca4a
Cyrildickson <cyrildickson147@gmail.com>
2024-02-02 09:42:05 +0000

    pitches fill

--- js/script.js
+++ js/script.js
@@ -8,7 +8,18 @@
 // })
 
 
-
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the current URL path
+    var path = window.location.pathname;
+  
+    // Find the corresponding link and add the 'active' class
+    var links = document.querySelectorAll('.nav-link');
+    links.forEach(function(link) {
+      if (link.getAttribute('href') === path) {
+        link.classList.add('active');
+      }
+    });
+  });
 
 
 
